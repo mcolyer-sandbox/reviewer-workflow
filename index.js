4a8085df003f5995646473b1aa3763fd8a164e1d
Matt Colyer <mcolyer@github.com>
2019-09-27 16:41:05 -0700

    Wait for responses

--- index.js
+++ index.js
@@ -14,30 +14,32 @@ try {
     console.log(JSON.stringify(team));
     console.log("")
 
-    console.log("Team: "+team["id"])
-    var members = octokit.teams.listMembers({team_id: team["id"]})
-    console.log(JSON.stringify(members))
-    for(var ii=0; ii < members.length; ii++) {
-      const member = members[ii]
-      console.log(JSON.stringify(member))
-    }
+    (async () => {
+      console.log("Team: "+team["id"])
+      var {data: members} = await octokit.teams.listMembers({team_id: team["id"]})
+      console.log(JSON.stringify(members))
+      for(var ii=0; ii < members.length; ii++) {
+        const member = members[ii]
+        console.log(JSON.stringify(member))
+      }
 
-    console.log("")
+      console.log("")
 
-    console.log("Adding "+members[0]["login"]+" as a reviewer")
-    octokit.pulls.createReviewRequest({
-        owner: repositoryOwner,
-        repo: repositoryName,
-        pull_number: pullRequestNumber,
-        reviewers: [members[0]["login"]]
-    })
+      console.log("Adding "+members[0]["login"]+" as a reviewer")
+      await octokit.pulls.createReviewRequest({
+          owner: repositoryOwner,
+          repo: repositoryName,
+          pull_number: pullRequestNumber,
+          reviewers: [members[0]["login"]]
+      })
 
-    console.log("Removing "+team["slug"]+" as a reviewer")
-    octokit.pulls.deleteReviewRequest({
-        owner: repositoryOwner,
-        repo: repositoryName,
-        pull_number: pullRequestNumber,
-        team_reviewers: [team["slug"]]
+      console.log("Removing "+team["slug"]+" as a reviewer")
+      await octokit.pulls.deleteReviewRequest({
+          owner: repositoryOwner,
+          repo: repositoryName,
+          pull_number: pullRequestNumber,
+          team_reviewers: [team["slug"]]
+      })
     })
   }
 } catch (error) {
